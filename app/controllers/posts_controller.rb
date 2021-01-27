d4fa1ef86cc60989820e93b6de51cd70ce050248
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-27 02:29:24 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,6 @@ class PostsController < ApplicationController
   # add create method here
   def create 
     @post = Post.create(params[:post])
-    redirect_to :'/posts/:id'
+    redirect_to :'/post/:id'
   end
 end
